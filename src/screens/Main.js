5435cad9a4858e26a1094ab37f148c18f2997252
Luigi Masini <luigi_masini@tiscali.it>
2019-09-14 16:19:29 +0200

    ljsHFEB

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {
     View,
-    Text
+    Text,
+    StyleSheet
 } from 'react-native';
+
+
+const styles = StyleSheet.create({
+	container: {
+		fontFamily: 'Cera Pro'
+	}
+});
 
 export default function Main(){
     return (
-        <View>
+        <View style={styles.container}>
             <Text>hello world!</Text>
         </View>
     )
